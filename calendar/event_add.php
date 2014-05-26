72fef4ced8942e34e500689c131f6f133b568bd3
Mantas Grubliauskis <mantas@creatur.lt>
2014-05-26 15:37:05 +0300

    Encoding bug
    weekdays: saturday/sunday: start ad 10:00, else at 16:00

--- calendar/event_add.php
+++ calendar/event_add.php
@@ -12,11 +12,11 @@ if (isset($_POST['submit']))
 	//susikonstruojam menesi
 	$menesis = $_POST['month'];
 	if($menesis<10) {$menesis = str_pad($menesis, 2, "0", STR_PAD_LEFT);}
-	
+
 	//susikonstruojam diena
 	$diena = $_POST['day'];
 	if($diena<10) {$diena = str_pad($diena, 2, "0", STR_PAD_LEFT);}
-	
+
 	$event_date = $_POST['year']."-".$menesis."-".$diena;
 	$event_time = $_POST['hour'] . ":" . $_POST['minute'];
 
@@ -26,51 +26,53 @@ if (isset($_POST['submit']))
 	$meilas = $_SESSION['user']['email'];
 	$user = $_SESSION['user']['username'];
 	$BookingId = mysql_insert_id();
-	send_mail($meilas,"Jधऎsधक registracija skrydधठiams",$pranesimas);
-		
+	send_mail($meilas,"Jūsų registracija skrydžiams",$pranesimas);
+
 	//redirect
 	header( 'Location: ../index.php?action=calendar' ) ;
 }
-else 
+else
 {
-	
+
 	$menesis = $_GET['month'];
 	if($menesis<10) {$menesis = str_pad($menesis, 2, "0", STR_PAD_LEFT);}
-	
+
 	//susikonstruojam diena
 	$diena = $_GET['day'];
 	if($diena<10) {$diena = str_pad($diena, 2, "0", STR_PAD_LEFT);}
-	
+
 	$event_date = $_GET['year']."-".$menesis."-".$diena;
+
+	$weekday = date('w', mktime(0, 0, 0, $_GET['month'], $_GET['day'], $_GET['year']));
 ?>
 
 <a class="b-close">[X]<a/>
 <form name="form1" method="post" action="calendar/event_add.php" class="calendar">
   <table border="0" cellspacing="0" cellpadding="0">
-  	
-    <tr> 
-      <td width="200" height="40" valign="top"><span class="addevent">Atvykimo diena:</span><br> 
+
+    <tr>
+      <td width="200" height="40" valign="top"><span class="addevent">Atvykimo diena:</span><br>
         <td height="40" valign="top">
   			<? echo $event_date; ?>
         </td>
     </tr>
-    <tr> 
-      <td width="200" height="40" valign="top"><span class="addevent">Atvykimo laikas:</span><br> 
+    <tr>
+      <td width="200" height="40" valign="top"><span class="addevent">Atvykimo laikas:</span><br>
         <span class="addeventextrainfo">(24val formatas)</span></td>
       <td height="40" valign="top">
-      	<input class="form-control" style="width: 50px; display: inline" name="hour" type="text" id="hour" value="10" size="2" maxlength="2"> : 
+      	<input class="form-control" style="width: 50px; display: inline" name="hour" type="text" id="hour" value="<?php echo $weekday == 0 || $weekday == 6 ? 10 : 16 ?>" size="2" maxlength="2"> :
       	<input class="form-control" style="width: 50px;display: inline" name="minute" type="text" id="minute" value="00" size="2" maxlength="2"><br />
       </td>
     </tr>
-    <tr> 
+    <tr>
       <td width="200" height="40" valign="top"><span class="addevent">Registracijos pastabos</span><br>
       	<span class="addeventextrainfo">Nurodykite, jei reikalinga nakvynė, skrydžiai į aikštelę ar pan.</span>
 	<span class="addeventextrainfo">Nurodykite kiek ir kokius skrydžius planuojate, esate instruktorius ar autoišvilktuvo operatorius.</span>
       	</td>
-      <td height="40" valign="top"> <textarea class="form-control" name="description" cols="18" rows="5" id="description"></textarea> 
+      <td height="40" valign="top"> <textarea class="form-control" name="description" cols="18" rows="5" id="description"></textarea>
       </td>
     </tr>
-    <tr> 
+    <tr>
       <td>&nbsp;</td>
       <td><br /><input class="btn btn-primary" name="submit" type="submit" id="submit" value="Registruotis"></td>
     </tr>
@@ -80,5 +82,5 @@ else
   <input name="month" type="hidden" value="<? echo $_GET['month']; ?>">
   <input name="day" type="hidden" value="<? echo $_GET['day']; ?>">
 </form>
-<?php 
-} 
+<?php
+}
