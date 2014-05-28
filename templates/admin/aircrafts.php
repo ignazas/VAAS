dcae057d8ff878c86f766d0d1417789a9fba695d
Ignas <ignas@igfe.eu>
2014-05-28 18:25:30 +0300

    Aircraft list visuals

--- templates/admin/aircrafts.php
+++ templates/admin/aircrafts.php
@@ -1,15 +1,37 @@
 <?php include "templates/include/header.php" ?>
 <?php include "templates/include/top-menu.php" ?>
 
+
 <div class="container">
 	<div class="page-header"><h1>Orlaivių redagavimas</h1></div>
 	<div class="col-md-8">
+		
+		<table class="table table-striped">
+      		<tr>
+			<th>Šaukinys</th>
+			<th>Modelis</th>
+			<th></th>
+			<th></th>
+          	</tr>
+          	
 			<?php 
 			foreach ( $ac as $aircraft) {
-				echo $aircraft['callsign'] . " / " . $aircraft['model']. "<br />"; 
-			}
-			
+				echo "<tr><td>" . $aircraft['callsign'] . "</td><td>" . $aircraft['model']. "</td><td>";  ?>
+				<form action="admin.php">
+          		<input type="hidden" name="bookingId" value="<?php echo $aircraft['callsign']; ?>"/>
+ 				<button type="submit" name="action" onclick="return confirm('Ar tikrai norite pašalinti orlaivį?')" class="btn btn-xs btn-danger" value="deleteAircraft">Pašalinti</button>
+				</form>
+				</td>
+				<td>
+				<form action="admin.php">
+          		<input type="hidden" name="bookingId" value="<?php echo $aircraft['callsign']; ?>"/>
+ 				<button type="submit" name="action" class="btn btn-xs btn-orig" value="editAircraft">Redaguoti</button>
+				</form>
+				</td>
+				</tr>
+			<?php }
 			?>
+		</table>
 	</div>
 </div> <!-- /container -->
 
