f619cf8a4a6f79999f082e3765a0922656f5e01f
Mantas Grubliauskis <mantas@creatur.lt>
2014-06-04 11:19:01 +0300

    Logoff before headers was sent and move to front page

--- templates/off.php
+++ templates/off.php
@@ -1,3 +1,23 @@
+<?php
+
+// First we execute our common code to connection to the database and start the session
+require ("common.php");
+
+// The parameter values
+$query_params = array(':username' => $_SESSION['user']['username']);
+
+//log it
+require_once dirname(__FILE__) . '/../functions.php';
+log_event($_SESSION['user']['username'], 'Leaves', '');
+
+// We remove the user's data from the session
+unset($_SESSION['user']);
+
+// We redirect them to the login page
+header("Location: index.php");
+die("Redirecting to: index.php");
+?>
+
 <?php include "templates/include/header.php"
 ?>
 <?php include "templates/include/top-menu.php"
@@ -10,26 +30,6 @@
 
 	<div class="col-md-4"></div>
 	<div class="col-md-4">
-		<?php
-
-        // First we execute our common code to connection to the database and start the session
-        require ("common.php");
-
-        // The parameter values
-        $query_params = array(':username' => $_SESSION['user']['username']);
-
-        //log it
-        require_once dirname(__FILE__) . '/../functions.php';
-        log_event($_SESSION['user']['username'], 'Leaves', '');
-
-        // We remove the user's data from the session
-
-        unset($_SESSION['user']);
-
-        // We redirect them to the login page
-        header("Location: index.php");
-        die("Redirecting to: index.php");
-		?>
 	</div>
 
 </div>
