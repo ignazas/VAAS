e787063eb09efde4407df4e0a736350f07a84382
Mantas Grubliauskis <mantas@creatur.lt>
2014-06-04 12:20:08 +0300

    removed not used variable

--- default.config.php
+++ default.config.php
@@ -12,7 +12,6 @@ else
 }
 
 define( "CATALOG", "vaas" );
-define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
 define( "HOMEPAGE_NUM_ARTICLES", 5 );
 
@@ -26,7 +25,7 @@ function handleException( $exception ) {
   echo "Sorry, a problem occurred. Please try later.";
   error_log( $exception->getMessage() );
 }
- 
+
 set_exception_handler( 'handleException' );
 
 require_once dirname(__FILE__) . '/helpers/strptime.inc';
