487a5cfb6a74f40f5023740ca1ed93bfb1c5bc09
Mantas Grubliauskis <mantas@creatur.lt>
2014-06-05 10:54:18 +0300

    Redirect user to his destination after login

--- templates/on.php
+++ templates/on.php
@@ -11,7 +11,7 @@
     // login form if they fail to enter the correct password.  It is initialized here
     // to an empty value, which will be shown if the user has not submitted the form.
     $submitted_username = '';
-    
+
     // This if statement checks to determine whether the login form has been submitted
     // If it has, then the login code is run, otherwise the form is displayed
     if(!empty($_POST))
@@ -26,12 +26,12 @@
             WHERE
                 username = :username
         ";
-        
+
         // The parameter values
         $query_params = array(
             ':username' => $_POST['username']
         );
-        
+
         try
         {
             // Execute the query against the database
@@ -41,15 +41,15 @@
         catch(PDOException $ex)
         {
             // Note: On a production website, you should not output $ex->getMessage().
-            // It may provide an attacker with helpful information about your code. 
+            // It may provide an attacker with helpful information about your code.
             die("Failed to run query: " . $ex->getMessage());
         }
-        
+
         // This variable tells us whether the user has successfully logged in or not.
         // We initialize it to false, assuming they have not.
         // If we determine that they have entered the right details, then we switch it to true.
         $login_ok = false;
-        
+
         // Retrieve the user data from the database.  If $row is false, then the username
         // they entered is not registered.
         $row = $stmt->fetch();
@@ -61,22 +61,22 @@
 
             //$check_password = hash('sha256', $_POST['password']);
             $check_password = md5($_POST['password']);
-			
+
             //for($round = 0; $round < 65536; $round++)
             //{
             //    $check_password = hash('sha256', $check_password);
             //}
-            
+
             if($check_password === $row['password'])
             {
                 // If they do, then we flip this to true
                 $login_ok = true;
             }
         }
-        
+
 		//getting ip
 		$ip = $_SERVER['REMOTE_ADDR'];
-				
+
         // If the user logged in successfully, then we send them to the private members-only page
         // Otherwise, we display a login failed message and show the login form again
         if($login_ok)
@@ -95,25 +95,26 @@
             $_SESSION['user'] = $row;
 
 			//log it
-			
+
 			$query = "INSERT INTO log(user, event, param) VALUES (:username,'Enters','".$ip."')";
 			$stmt = $db->prepare($query);
             $result = $stmt->execute($query_params);
-			
+
             // Redirect the user to the private members-only page.
-            header("Location: index.php");
+	    $destination = isset($_GET['destination']) ? $_GET['destination'] : 'index.php';
+            header("Location: $destination");
             die("Redirecting to: index.php");
         }
         else
         {
             // Tell the user they failed
             echo "<div class=\"alert alert-danger\"><strong>Klaida!</strong> Prisijungti nepavyko.</div>";
-			
+
 			//log it
             $query = "INSERT INTO log(user, event, param) VALUES (:username,'Knocks','".$ip."')";
 			$stmt = $db->prepare($query);
             $result = $stmt->execute($query_params);
-			
+
             // Show them their username again so all they have to do is enter a new
             // password.  The use of htmlentities prevents XSS attacks.  You should
             // always use htmlentities on user submitted values before displaying them
@@ -122,7 +123,7 @@
             $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8');
         }
     }
-    
+
 ?>
 <link href="css/signin.css" rel="stylesheet">
 <form class="form-signin" role="form" action="" method="post">
@@ -137,7 +138,7 @@
 
 
 
-		
+
 </div> <!-- /container -->
 
 <?php include "templates/include/footer.php" ?>
