ce3579b15a7ddab28205033950861ac4dddacca0
mangru <mantas@kurti.lt>
2014-06-19 01:00:09 +0300

    Show tomorrows plans too

--- js/scripts.js
+++ js/scripts.js
@@ -19,92 +19,89 @@ jQuery( document ).ready(function( $ ) {
     });
 
 
-    $('#dienos').on('click', 'a.add', function(event) {
-
-        // Prevents the default action to be triggered. 
-        event.stopPropagation();
-    	event.preventDefault();
-	var data = $(this).attr('href');
+    $('#dienos,.flight-plan')
+	.on('click', 'a.add', function(event) {
+            // Prevents the default action to be triggered. 
+            event.stopPropagation();
+    	    event.preventDefault();
+	    var data = $(this).attr('href');
         
-        // Triggering bPopup when click event is fired
-        $('#registruotis').bPopup({
-            modalClose: true,
-            opacity: 0.6,
-            positionStyle: 'fixed', //'fixed' or 'absolute'
-            fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            followSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            loadUrl: 'calendar/event_add.php'+data, //Uses jQuery.load()
-        });		
+            // Triggering bPopup when click event is fired
+            $('#registruotis').bPopup({
+		modalClose: true,
+		opacity: 0.6,
+		positionStyle: 'fixed', //'fixed' or 'absolute'
+		fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		followSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		loadUrl: 'calendar/event_add.php'+data, //Uses jQuery.load()
+            });		
 	
-    });
-    $('#dienos').on('click', 'a.registracija, a.talka, a.šventė, a.svečiai, a.kita', function(event) {
-
-        // Prevents the default action to be triggered. 
-        event.stopPropagation();
-    	event.preventDefault();
-	var data = $(this).attr('href');
+	})
+	.on('click', 'a.registracija, a.talka, a.šventė, a.svečiai, a.kita', function(event) {
+            // Prevents the default action to be triggered. 
+            event.stopPropagation();
+    	    event.preventDefault();
+	    var data = $(this).attr('href');
         
-        // Triggering bPopup when click event is fired
-        $('#registracija').bPopup({
-            modalClose: true,
-            opacity: 0.6,
-            positionStyle: 'fixed', //'fixed' or 'absolute'
-            fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            followSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            loadUrl: 'calendar/event.php'+data, //Uses jQuery.load()
-        });		
-    });
-
-    $('#dienos').on('click', 'a.add_day', function(event) {
-
-        // Prevents the default action to be triggered. 
-        event.stopPropagation();
-    	event.preventDefault();
-	var data = $(this).attr('href');
+            // Triggering bPopup when click event is fired
+            $('#registracija').bPopup({
+		modalClose: true,
+		opacity: 0.6,
+		positionStyle: 'fixed', //'fixed' or 'absolute'
+		fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		followSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		loadUrl: 'calendar/event.php'+data, //Uses jQuery.load()
+            });		
+	})
+	.on('click', 'a.add_day', function(event) {
+            // Prevents the default action to be triggered. 
+            event.stopPropagation();
+    	    event.preventDefault();
+	    var data = $(this).attr('href');
         
-        // Triggering bPopup when click event is fired
-        $('#addDay').bPopup({
-            modalClose: true,
-            opacity: 0.6,
-            positionStyle: 'fixed', //'fixed' or 'absolute'
-            fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            followSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            loadUrl: 'calendar/day_add.php'+data, //Uses jQuery.load()
-        });
-    });
-    $('#dienos').on('click', 'a.show_day', function(event) {
-
-        // Prevents the default action to be triggered. 
-        event.stopPropagation();
-    	event.preventDefault();
-	var data = $(this).attr('href');
+            // Triggering bPopup when click event is fired
+            $('#addDay').bPopup({
+		modalClose: true,
+		opacity: 0.6,
+		positionStyle: 'fixed', //'fixed' or 'absolute'
+		fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		followSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		loadUrl: 'calendar/day_add.php'+data, //Uses jQuery.load()
+            });
+	})
+	.on('click', 'a.show_day', function(event) {
+            // Prevents the default action to be triggered. 
+            event.stopPropagation();
+    	    event.preventDefault();
+	    var data = $(this).attr('href');
         
-        // Triggering bPopup when click event is fired
-        $('#report').bPopup({
-            modalClose: true,
-            opacity: 0.6,
-            positionStyle: 'fixed', //'fixed' or 'absolute'
-            fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            followSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            loadUrl: 'calendar/day.php'+data, //Uses jQuery.load()
-        });				
+            // Triggering bPopup when click event is fired
+            $('#report').bPopup({
+		modalClose: true,
+		opacity: 0.6,
+		positionStyle: 'fixed', //'fixed' or 'absolute'
+		fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		followSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		loadUrl: 'calendar/day.php'+data, //Uses jQuery.load()
+            });				
 
-    });
-    $('#knopkes').on('click', '#addAircraft', function(event) {
+	});
 
-        // Prevents the default action to be triggered. 
-        event.stopPropagation();
-    	event.preventDefault();
-	
-        // Triggering bPopup when click event is fired
-        $('#addAircraftDialog').bPopup({
-            modalClose: true,
-            opacity: 0.6,
-            positionStyle: 'fixed', //'fixed' or 'absolute'
-            fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            followSpeed: 'slow', //can be a string ('slow'/'fast') or int
-            loadUrl: 'views/aircrafts/add_aircraft.php', //Uses jQuery.load()
-        });				
+    $('#knopkes')
+	.on('click', '#addAircraft', function(event) {
+            // Prevents the default action to be triggered. 
+            event.stopPropagation();
+    	    event.preventDefault();
+	    
+            // Triggering bPopup when click event is fired
+            $('#addAircraftDialog').bPopup({
+		modalClose: true,
+		opacity: 0.6,
+		positionStyle: 'fixed', //'fixed' or 'absolute'
+		fadeSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		followSpeed: 'slow', //can be a string ('slow'/'fast') or int
+		loadUrl: 'views/aircrafts/add_aircraft.php', //Uses jQuery.load()
+            });				
 
-    });
+	});
 });
--- views/dashboard/block_calendar.php
+++ views/dashboard/block_calendar.php
@@ -1,13 +1,14 @@
-
-
-<div class="col-md-6" id="dienos">
   <link href="css/cal.css" rel="stylesheet" type="text/css">
   <div id="registracija"></div>
   <div id="registruotis"></div>
+
+
+<div class="col-md-6 flight-plan">
   <h2>Šiandien skrenda</h2>
+  <?php $today = time(); ?>
   <ul class="list-group">
-	<?php if (!empty($elements['calendar']['events']['results'])) { ?>
-    <?php foreach ($elements['calendar']['events']['results'] as $event) { $title = $event->event_time . ' ' . ($event->event_type != 'registracija' ? $event->event_desc : $event->event_title); ?>
+	<?php if (!empty($elements['calendar-today']['events']['results'])) { ?>
+    <?php foreach ($elements['calendar-today']['events']['results'] as $event) { $title = $event->event_time . ' ' . ($event->event_type != 'registracija' ? $event->event_desc : $event->event_title); ?>
     <li class="list-group-item">
       <a class="<?php echo $event->event_type ?>" href="?id=<?php echo $event->event_id ?>" title="<?php echo $title ?>"><?php echo $title ?></a>
 <?php if ($event->event_type != 'registracija') { ?>
@@ -15,14 +16,41 @@
 <?php } ?>
     </li>
     <?php } } else { echo "<li class=\"list-group-item\">Registracijų nėra.</li>";} ?>
-	<?php IF (!empty($elements['calendar']['days']['results'])) { ?>
-    <?php foreach ($elements['calendar']['days']['results'] as $day) { ?>
+	<?php IF (!empty($elements['calendar-today']['days']['results'])) { ?>
+    <?php foreach ($elements['calendar-today']['days']['results'] as $day) { ?>
     <li class="<?php echo $day->status ?> alert <?php echo $day->status == 'nevyksta' ? 'alert-danger' : 'alert-info' ?>"><?php echo $day->reason ?></li>
     <?php } } ?>
   </ul>
   <div class="buttons">
-    <a class="add btn btn-sm btn-primary" href="?day=<?php echo date('j', time()) ?>&amp;month=<?php echo date('n', time()) ?>&amp;year=<?php echo date('Y', time()) ?>&amp;destination=<?php echo $_SERVER['REQUEST_URI'] ?>"><i class="glyphicon glyphicon-plus"></i> Šiandien</a>
-    <?php $bookingIdList = array(); if (!empty($elements['calendar']['events']['results'])) foreach ($elements['calendar']['events']['results'] as $event) if (!empty($_SESSION['user']['id']) && $event->user_id == $_SESSION['user']['id']) $bookingIdList[] = $event->event_id; ?>
+    <a class="add btn btn-sm btn-primary" href="?day=<?php echo date('j', $today) ?>&amp;month=<?php echo date('n', $today) ?>&amp;year=<?php echo date('Y', $today) ?>&amp;destination=<?php echo $_SERVER['REQUEST_URI'] ?>"><i class="glyphicon glyphicon-plus"></i> Šiandien</a>
+    <?php $bookingIdList = array(); if (!empty($elements['calendar-today']['events']['results'])) foreach ($elements['calendar-today']['events']['results'] as $event) if (!empty($_SESSION['user']['id']) && $event->user_id == $_SESSION['user']['id']) $bookingIdList[] = $event->event_id; ?>
+    <?php if (!empty($bookingIdList)) { ?>
+    <a class="delete btn btn-danger" onclick="return confirm('Ar tikrai norite atsisakyti registracijos?')" href="index.php?action=deleteBooking&amp;bookingId=<?php echo implode(',', $bookingIdList) ?>&amp;destination=<?php echo $_SERVER['REQUEST_URI'] ?>"><i class="glyphicon glyphicon-minus"></i> Atsisakyti</a>
+    <?php } ?>
+  </div>
+</div>
+
+<div class="col-md-6 flight-plan">
+  <h2>Rytoj skrenda</h2>
+  <ul class="list-group">
+	<?php if (!empty($elements['calendar-tomorrow']['events']['results'])) { ?>
+    <?php foreach ($elements['calendar-tomorrow']['events']['results'] as $event) { $title = $event->event_time . ' ' . ($event->event_type != 'registracija' ? $event->event_desc : $event->event_title); ?>
+    <li class="list-group-item">
+      <a class="<?php echo $event->event_type ?>" href="?id=<?php echo $event->event_id ?>" title="<?php echo $title ?>"><?php echo $title ?></a>
+<?php if ($event->event_type != 'registracija') { ?>
+      <span class="badge"><?php echo $event->event_type ?></span>
+<?php } ?>
+    </li>
+    <?php } } else { echo "<li class=\"list-group-item\">Registracijų nėra.</li>";} ?>
+	<?php IF (!empty($elements['calendar-tomorrow']['days']['results'])) { ?>
+    <?php foreach ($elements['calendar-tomorrow']['days']['results'] as $day) { ?>
+    <li class="<?php echo $day->status ?> alert <?php echo $day->status == 'nevyksta' ? 'alert-danger' : 'alert-info' ?>"><?php echo $day->reason ?></li>
+    <?php } } ?>
+  </ul>
+  <div class="buttons">
+    <?php $tomorrow = $today + 86400; ?>
+    <a class="add btn btn-sm btn-primary" href="?day=<?php echo date('j', $tomorrow) ?>&amp;month=<?php echo date('n', $tomorrow) ?>&amp;year=<?php echo date('Y', $tomorrow) ?>&amp;destination=<?php echo $_SERVER['REQUEST_URI'] ?>"><i class="glyphicon glyphicon-plus"></i> Rytoj</a>
+    <?php $bookingIdList = array(); if (!empty($elements['calendar-tomorrow']['events']['results'])) foreach ($elements['calendar-tomorrow']['events']['results'] as $event) if (!empty($_SESSION['user']['id']) && $event->user_id == $_SESSION['user']['id']) $bookingIdList[] = $event->event_id; ?>
     <?php if (!empty($bookingIdList)) { ?>
     <a class="delete btn btn-danger" onclick="return confirm('Ar tikrai norite atsisakyti registracijos?')" href="index.php?action=deleteBooking&amp;bookingId=<?php echo implode(',', $bookingIdList) ?>&amp;destination=<?php echo $_SERVER['REQUEST_URI'] ?>"><i class="glyphicon glyphicon-minus"></i> Atsisakyti</a>
     <?php } ?>
