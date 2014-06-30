d5caf37c113a77f76d7d2d30e3aacce6bed6f8a7
Mantas Grubliauskis <mantas@creatur.lt>
2014-06-30 08:18:29 +0300

    Changed Trakai from Paluknis (because it is wrong Paluknis - neat Utena)

--- views/dashboard/block_meteo.php
+++ views/dashboard/block_meteo.php
@@ -1,12 +1,11 @@
 <?php
-$file = dirname(__FILE__) . "/../../uploads/meteo_paluknis.png";
+$file = dirname(__FILE__) . "/../../uploads/meteo_trakai.png";
 if (!file_exists($file) || round(abs(time() - filemtime($file)) / 60, 2) > 10) { // min
-   file_put_contents($file, file_get_contents('http://meteo.lt/dokumentai/operatyvi_inf/skaitmenine_prog/meteo_paluknis.png'));
+   file_put_contents($file, file_get_contents('http://meteo.lt/dokumentai/operatyvi_inf/skaitmenine_prog/meteo_trakai.png'));
 }
 ?>
 <div class="col-md-8">
   <h2>Orų prognozė</h2>
-<?php //<img src="http://meteo.lt/dokumentai/operatyvi_inf/skaitmenine_prog/meteo_paluknis.png" alt="Skaitmeninė Paluknio orų prognozė" /> ?>
-  <img class="img-responsive" src="/<?php echo CATALOG ?>/uploads/meteo_paluknis.png" alt="Skaitmeninė Paluknio orų prognozė" />
+  <img class="img-responsive" src="/<?php echo CATALOG ?>/uploads/meteo_trakai.png" alt="Skaitmeninė Trakų orų prognozė" />
   <a href="http://meteo.lt/skaitmenine_prog_lt_zem.php?skpt=lt_krit" target="_blank">Daugiau</a>
 </div>
