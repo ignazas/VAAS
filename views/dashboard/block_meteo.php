cda676ffdcc7c8cdf4556c30dbcf6702aaf84942
Mantas Grubliauskis <mantas@creatur.lt>
2014-07-17 11:36:31 +0300

    Styles (cherry picked from commit 6d7ce1d99b3892c29f0c827e07926ac459b73fd2)

--- views/dashboard/block_meteo.php
+++ views/dashboard/block_meteo.php
@@ -4,8 +4,7 @@ if (!file_exists($file) || round(abs(time() - filemtime($file)) / 60, 2) > 10) {
    file_put_contents($file, file_get_contents('http://meteo.lt/dokumentai/operatyvi_inf/skaitmenine_prog/meteo_trakai.png'));
 }
 ?>
-<div class="col-md-8">
+<div class="col-md-12">
   <h2>Orų prognozė</h2>
-  <img class="img-responsive" src="/<?php echo CATALOG ?>/uploads/meteo_trakai.png" alt="Skaitmeninė Trakų orų prognozė" />
-  <a href="http://meteo.lt/skaitmenine_prog_lt_zem.php?skpt=lt_krit" target="_blank">Daugiau</a>
+  <a href="index.php?action=weather"><img class="img-responsive" src="/<?php echo CATALOG ?>/uploads/meteo_trakai.png" alt="Skaitmeninė Trakų orų prognozė" /><a>
 </div>
--- views/dashboard/view.php
+++ views/dashboard/view.php
@@ -3,9 +3,11 @@
   <p>Tai yra Vilniaus Aeroklubo Administravimo Sistema - VAAS.</p>
 </div>
 
-<div class="row">
+<div class="col-md-8">
   <?php include 'block_calendar.php'; ?>
-  <?php include 'block_article.php'; ?>
   <?php include 'block_meteo.php'; ?>
+</div>
+<div class="col-md-4">
+  <?php include 'block_article.php'; ?>
   <?php include 'block_quote.php'; ?>
 </div>
