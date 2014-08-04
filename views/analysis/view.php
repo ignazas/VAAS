dd656b32182ef0b1ae021f6542fcb4ecc3c39bc2
Mantas Grubliauskis <mantas@creatur.lt>
2014-08-04 13:14:34 +0300

    Added icg review

---
+++ views/analysis/view.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<div class="page-header"><h1>Skrydžių analizė</h1></div>
+<div class="row">
+  <div class="col-md-8">
+    <?php echo isset($results['google_maps_code']) ? $results['google_maps_code'] : NULL; ?>
+  </div>
+
+  <div class="col-md-4">
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+	<h3 class="panel-title">Info</h3>
+      </div>
+      <div class="panel-body">
+	<p>Čia yra talpinami Jūsų skrydžių analizės failai.</p>
+	<ul>
+<?php foreach ($results['files'] as $file) { ?>
+          <li><a href="?action=analysis&amp;file=<?php echo urlencode(basename($file, '.igc')) ?>"><?php echo basename($file, '.igc') ?></a> <a href="?action=analysis&amp;view=Delete&amp;file=<?php echo urlencode(basename($file, '.igc')) ?>"><i class="glyphicon glyphicon-remove"></i></a></li>
+<?php } ?>
+	</ul>
+        <form action="" method="post" enctype="multipart/form-data">
+	  <input type="file" id="file" name="files[]" multiple="multiple" />
+	  <input type="submit" value="Upload!" />
+	</form>
+      </div>
+    </div>
+  </div>
+</div>
