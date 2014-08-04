8be20e6c89899927d9cf66bbbef450415f13f71d
Mantas Grubliauskis <mantas@creatur.lt>
2014-08-04 13:58:41 +0300

    Codereview

--- views/analysis/view.php
+++ views/analysis/view.php
@@ -14,12 +14,14 @@
 	<h3 class="panel-title">Info</h3>
       </div>
       <div class="panel-body">
-	<p>Čia yra talpinami Jūsų skrydžių analizės failai.</p>
+	<p>Čia yra talpinami Jūsų skrydžių analizės failai (*.igc).</p>
+<?php if (!empty($results['files'])) { ?>
 	<ul>
-<?php if (!empty($results['files'])) foreach ($results['files'] as $file) { ?>
+<?php   foreach ($results['files'] as $file) { ?>
           <li><a href="?action=analysis&amp;file=<?php echo urlencode(basename($file, '.igc')) ?>"><?php echo basename($file, '.igc') ?></a> <a href="?action=analysis&amp;view=Delete&amp;file=<?php echo urlencode(basename($file, '.igc')) ?>"><i class="glyphicon glyphicon-remove"></i></a></li>
-<?php } ?>
+<?php   } ?>
 	</ul>
+<?php } ?>
         <form action="" method="post" enctype="multipart/form-data">
 	  <input type="file" id="file" name="files[]" multiple="multiple" />
 	  <input type="submit" value="Upload!" />
