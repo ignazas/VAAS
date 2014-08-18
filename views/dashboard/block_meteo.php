b3eb85fccabe1df810249371f8ba4cf99648c55a
mangru <mantas@kurti.lt>
2014-08-18 22:03:11 +0300

    Use valkininkai instead of trakai

--- views/dashboard/block_meteo.php
+++ views/dashboard/block_meteo.php
@@ -1,5 +1,5 @@
 <?php
-$file = dirname(__FILE__) . "/../../uploads/meteo_trakai.png";
+$file = dirname(__FILE__) . "/../../uploads/meteo_valkininkai.png";
 if (!file_exists($file) || round(abs(time() - filemtime($file)) / 60, 2) > 30) { // min
   $opts = array('http' => array(
             'method'  => 'GET',
@@ -7,7 +7,7 @@ if (!file_exists($file) || round(abs(time() - filemtime($file)) / 60, 2) > 30) {
           )
   );
   $context  = stream_context_create($opts);
-  $content = @file_get_contents('http://meteo.lt/dokumentai/operatyvi_inf/skaitmenine_prog/meteo_trakai.png', false, $context, -1);
+  $content = @file_get_contents('http://meteo.lt/dokumentai/operatyvi_inf/skaitmenine_prog/meteo_valkininkai.png', false, $context, -1);
   if (!empty($content))
     file_put_contents($file, $content);
 }
@@ -16,6 +16,6 @@ if (!file_exists($file) || round(abs(time() - filemtime($file)) / 60, 2) > 30) {
 <?php if (file_exists($file)) { ?>
 <div class="row">
   <h2>Orų prognozė</h2>
-  <a href="index.php?action=weather"><img class="img-responsive" src="/<?php echo CATALOG ?>/uploads/meteo_trakai.png" alt="Skaitmeninė Trakų orų prognozė" /><a>
+  <a href="index.php?action=weather"><img class="img-responsive" src="/<?php echo CATALOG ?>/uploads/meteo_valkininkai.png" alt="Skaitmeninė Valkininkų orų prognozė" /><a>
 </div>
 <?php } ?>
