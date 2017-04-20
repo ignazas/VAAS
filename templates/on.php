e9784c32ab9ebef18872af93382db0b6c176c9af
Mantas Grubliauskis <mantas@creatur.lt>
2017-04-20 10:55:53 +0300

    Updated login form

--- templates/on.php
+++ templates/on.php
@@ -16,10 +16,20 @@ UserHelper::log_in();
 
     <form class="form-signin" role="form" action="" method="post">
       <h2 class="form-signin-heading">Prisijunkite</h2>
-      <input class="form-control" required="" placeholder="El. paštas" type="text" name="username" value="<?php echo !empty($_POST['username']) ? htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8') : NULL ?>" autofocus="" />
-      <input class="form-control" required="" placeholder="Slaptažodis" type="password" name="password" value="" />
+	  <div class="form-group">
+        <label for="username">Naudotojo vardas arba el. paštas</label>
+        <input class="form-control" required="" placeholder="Naudotojo vardas arba el. paštas" type="text" id="username" name="username" value="<?php echo !empty($_POST['username']) ? htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8') : NULL ?>" autofocus="" />
+      </div>
+	  <div class="form-group">
+        <label for="password">Slaptažodis</label>
+        <input class="form-control" required="" placeholder="Slaptažodis" type="password" id="password" name="password" value="" />
+      </div>
+
       <a href="#" id="reminder">Atsiųsti naują slaptažodį</a>
-      <br /><br />
+
+      <br />
+      <br />
+
       <input class="btn btn-lg btn-primary btn-block" type="submit" value="Prisijungti" />
     </form>
 </div> <!-- /container -->
