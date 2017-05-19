3312430191b968195a18cda8f95ca109cd78dab8
Mantas Grubliauskis <mantas@creatur.lt>
2017-05-19 13:02:22 +0300

    Updated camera - get token

--- views/dashboard/block_cam.php
+++ views/dashboard/block_cam.php
@@ -1,6 +1,29 @@
+<?php
+  function GetAddress() {
+    $opts = array('http' => array(
+                'method'  => 'GET',
+                'timeout' => 5
+            )
+    );
+    $context  = stream_context_create($opts);
+    $content = @file_get_contents("http://ipcamlive.com/paluknyscam1", false, $context, -1);
+    if (!empty($content)) {
+      preg_match_all("/var token = '([^']+)';/s", $content, $matches);
+      if (!empty($matches[1][0])) {
+        $token = $matches[1][0];
+        return "http://ipcamlive.com/player/player.php?alias=paluknyscam1&amp;autoplay=1&amp;disablevideofit=1&amp;token=$token&amp;debug=1";
+      }
+    }
+    return ;
+  }
+  $videoAddress = GetAddress();
+?>
+
+<?php if (!empty($videoAddress)) { ?>
+
 <div class="row">
   <h2>Paluknio aerodromas</h2>
-  <iframe id="playeriframe" src="http://ipcamlive.com/player/player.php?alias=paluknyscam1&amp;autoplay=1&amp;disablevideofit=1&amp;token=9506138d&amp;debug=1" width="1024px" height="838px" frameborder="0" allowfullscreen=""></iframe>
+  <iframe id="playeriframe" src="<?php echo $videoAddress ?>" width="1024px" height="838px" frameborder="0" allowfullscreen=""></iframe>
   <script>
 $( document ).ready(function() {
   function UpdateWidth() {
@@ -19,3 +42,5 @@ $( document ).ready(function() {
 });
   </script>
 </div>
+
+<?php } ?>
